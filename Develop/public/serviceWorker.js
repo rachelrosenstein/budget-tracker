4485aa5f02832db6ddfbd1f4fab2eb2d8814305f
Rachel Rosenstein <rachelrosenstein1@gmail.com>
2021-01-14 15:39:31 -0500

    service worker updated

---
+++ Develop/public/serviceWorker.js
@@ -0,0 +1,13 @@
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/styles.css",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
