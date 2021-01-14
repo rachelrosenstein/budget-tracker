b91ae6d5cd535d9e88579b248d55ab7856e941e3
Rachel Rosenstein <rachelrosenstein1@gmail.com>
2021-01-14 16:07:25 -0500

    deploying try 1

--- Develop/public/serviceWorker.js
+++ Develop/public/serviceWorker.js
@@ -1,16 +1,18 @@
 
-const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png",
-    "/index.js",
-    "/manifest.webmanifest",
-    "/styles.css",
-];
-
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+const iconSizes = ["192", "512"];
+const iconFiles = iconSizes.map(
+    (size) => `/icons/icon-${size}x${size}.png`
+);
+
+const staticFilesToPreCache = [
+    "/",
+    "/index.js",
+    "/favicon.ico",
+    "/manifest.webmanifest",
+].concat(iconFiles);
 
 self.addEventListener("install", function (evt) {
     evt.waitUntil(
